b0b9ac883f8b0f48446fad0c4132cb6af1789506
wschafer <wschafer@student.42berlin.de>
2025-09-29 16:01:09 +0200

    refactor main function and add data structure for process management

--- main.c
+++ main.c
@@ -3,50 +3,77 @@
 
 int	main(int argc, char **argv, char **environ)
 {
-	int		fd[2];
-	int		fd_in;
-	int		fd_out;
-	pid_t	cpid[2];
-	char	*path;
-	char	**args;
-	int		i;
-
-	i = 2;
+	t_data	*data;
+	int		arg;
+	
+	data = ft_calloc(1, sizeof(t_data));
+		// if (!data)
+		// 	error ();
+	data->fd = 0;
+	data->file1 = argv[1];
+	data->file2 = argv[argc - 1];
+	make_pipes(data, argc);
+	arg = 2;
 	if (argc >= 5)
 	{
-		pipe(fd);
-		cpid[0] = fork();
-		if (cpid[0] == 0 && i == 2)
+		data->cpid[0] = fork();
+		if (data->cpid[0] == 0 && arg == 2)
+			exec_left_arg(data, argv[arg], environ);
+		arg++;
+
+		data->cpid[1] = fork();
+		if (data->cpid[1] == 0 && arg == argc - 2)
+			exec_right_arg(data, argv[arg], environ);
+
+		for (int i = 0; i < argc - 4; i++)
 		{
-			fd_in = open_infile(argv[1]);
-			dup2(fd_in, STDIN_FILENO);
-			dup2(fd[1], STDOUT_FILENO);
-			close_fd(fd_in, fd[0], fd[1]);
-			path = get_path(argv[2], environ);
-			printf("%s", path);
-			args = get_args(argv[2]);
-			execve(path, args, environ);
-			
+			close(data->fds[i * 2]);
+			close(data->fds[i * 2 + 1]);
 		}
-		i++;
-		cpid[1] = fork();
-		if (cpid[1] == 0 && i == 3)
-		{
-			fd_out = open_outfile(argv[argc - 1]);
-			dup2(fd_out, STDOUT_FILENO);
-			dup2(fd[0], STDIN_FILENO);
-			close_fd(fd_out, fd[0], fd[1]);
-			path = get_path(argv[3], environ);
-			args = get_args(argv[3]);
-			execve(path, args, environ);
-		}
-		close(fd[0]);
-		close(fd[1]);
 		for (int i = 0; i < 2; i++)
 			wait(NULL);
 	}
 	// else
 	// 	error();
+}
+
+void	make_pipes(t_data *data, int argc)
+{
+	int	i;
+
+	data->fds = ft_calloc(argc - 4, sizeof(int));
+	// if (!data->fds)
+	// 	error();
+	i = 0;
+	while (i < argc - 4)
+	{
+		pipe(&data->fds[i * 2]);
+		i++;
+	}
+	for (int i = 0; i < (argc - 4) * 2; i++)
+		printf("fd %d = %d\n", i, data->fds[i]);
+}
+
+void	exec_left_arg(t_data *data, char *arg, char **environ)
+{
+	data->fd_in = open_infile(data->file1);
+	dup2(data->fd_in, STDIN_FILENO);
+	dup2(data->fds[data->fd * 2 + 1], STDOUT_FILENO);
+	close_fd(data->fd_in, data->fds[data->fd * 2], data->fds[1]);
+	data->path = get_path(arg, environ);
+	data->args = get_args(arg);
+	execve(data->path, data->args, environ);
+}
+
+void	exec_right_arg(t_data *data, char *arg, char **environ)
+{
+	data->fd_out = open_outfile(data->file2);
+	dup2(data->fd_out, STDOUT_FILENO);
+	dup2(data->fds[data->fd * 2], STDIN_FILENO);
+	close_fd(data->fd_out, data->fds[0], data->fds[1]);
+	data->path = get_path(arg, environ);
+	data->args = get_args(arg);
+	execve(data->path, data->args, environ);
 }
 
 int	open_infile(char *file1)
--- pipex.h
+++ pipex.h
@@ -8,6 +8,19 @@
 # include "libft/libft.h"
 # include <sys/wait.h>
 
+typedef struct s_data
+{
+	int		fd_in;
+	int		fd_out;
+	char	*path;
+	char	**args;
+	char	*file1;
+	char	*file2;
+	int		*fds;
+	int		fd;
+	pid_t	cpid[2];
+}	t_data;
+
 int		main(int argc, char **argv, char **environ);
 char	*get_path(char *command, char **environ);
 char	**get_pathes(char **environ);
@@ -15,6 +28,8 @@ char	**get_args(char *command);
 int		open_infile(char *file1);
 int		open_outfile(char *file2);
 int		close_fd(int n, ...);
-
+void	exec_left_arg(t_data *data, char *arg, char **environ);
+void	exec_right_arg(t_data *data, char *arg, char **environ);
+void	make_pipes(t_data *data, int argc);
 
 #endif
